07e665beb2e5f16b799739ec8b0bb9c512b76b93
amgstrider <amgstrider@gmail.com>
2024-03-11 16:51:04 +0400

    feat: open api

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yamljs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,10 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  const swaggerDocument = YAML.load('./doc/api.yaml');
+
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
